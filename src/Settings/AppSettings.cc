c085b402eb06dde003abb598d7feaedb5b403571
Wildan Fadillah <68156696+RookieNumber@users.noreply.github.com>
2025-09-14 00:18:23 +0700

    initiate translation

--- src/Settings/AppSettings.cc
+++ src/Settings/AppSettings.cc
@@ -31,6 +31,7 @@ QList<QLocale::Language> AppSettings::_rgReleaseLanguages = {
     QLocale::Korean,
     QLocale::Portuguese,
     QLocale::Russian,
+    QLocale::Indonesian
 };
 
 // Partial languages are 40%+ complete
@@ -59,7 +60,8 @@ AppSettings::LanguageInfo_t AppSettings::_rgLanguageInfo[] = {
     { QLocale::Russian,         "Pусский (Russian)" },
     { QLocale::Spanish,         "Español (Spanish)" },
     { QLocale::Swedish,         "Svenska (Swedish)" },
-    { QLocale::Turkish,         "Türk (Turkish)" }
+    { QLocale::Turkish,         "Türk (Turkish)" },
+    { QLocale::Indonesian,      "Bahasa Indonesia (Indonesian)" }
 };
 
 DECLARE_SETTINGGROUP(App, "")
