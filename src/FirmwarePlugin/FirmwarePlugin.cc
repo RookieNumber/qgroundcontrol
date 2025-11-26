a2103d50f820ea071aadf054a66118307804611d
Wildan Fadillah <68156696+RookieNumber@users.noreply.github.com>
2025-11-26 17:06:22 +0700

    comment out currType variable

--- src/FirmwarePlugin/FirmwarePlugin.cc
+++ src/FirmwarePlugin/FirmwarePlugin.cc
@@ -1083,7 +1083,7 @@ void FirmwarePlugin::_versionFileDownloadFinished(QString& remoteFile, QString& 
 
     qCDebug(FirmwarePluginLog) << "Latest stable version = "  << version;
 
-    int currType = vehicle->firmwareVersionType();
+    // int currType = vehicle->firmwareVersionType();
 
     // Check if lower version than stable or same version but different type
     // if (currType == FIRMWARE_VERSION_TYPE_OFFICIAL && vehicle->versionCompare(version) < 0) {
